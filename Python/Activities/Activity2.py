2c7cd473cb4a39b7c14933a4495478023fdd6422
nidhi0894 <78132162+nidhi0894@users.noreply.github.com>
2022-05-13 12:23:33 +0530

    Updated

--- Python/Activities/Activity2.py
+++ Python/Activities/Activity2.py
@@ -4,9 +4,3 @@ if number % 2==0 :
     print("Entered Number is Even")
 else:
      print("Entered Number is ODD")
-# num = int(input("Enter a number: "))
-# mod = num % 2
-# if mod > 0:
-#     print("You picked an odd number.")
-# else:
-#     print("You picked an even number.")
